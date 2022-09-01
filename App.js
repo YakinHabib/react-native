c419cefa9c469046dc38cc7a221be1355332ca14
YakinHabib <yakinhabibharahap@gmail.com>
2022-09-01 14:20:53 +0700

    Komponen dasar 1.1

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, View, Image, TextInput} from 'react-native';
 
 const App = () => {
-    return <Text>Yakin habib</Text>
+    return (
+        <View>
+            <Text> Halo Dunia, Saya Yakin Habib</Text>
+            <Ket/>
+            <Photo/>
+            <Text> </Text>
+            <Text> Tulis sesuatu dibawah ini</Text>
+            <TextInput style={{borderWidth: 1}} />
+        </View>
+    );
+};
 
-}
+const Ket = () => {
+    return <Text> Dibawah ini merupakan gambar dari arsitektur bangunan</Text>
+};
+
+const Photo = () => {
+    return (
+        <Image 
+            source={{uri: 'https://placeimg.com/100/100/arch'}}
+            style= {{width: 100, height: 100}}
+        />
+    );
+};
 
 export default App;
