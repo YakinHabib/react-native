eab5af3919d4abb83b0fa6dd5f0f0c02eba3e9dd
YakinHabib <yakinhabibharahap@gmail.com>
2022-09-01 18:06:24 +0700

    Styling Card Product

--- App.js
+++ App.js
@@ -1,82 +1,76 @@
 import React, { Component } from 'react';
 import {Text, View, Image, TextInput, StyleSheet} from 'react-native';
+//import {style} from './style';
 
 const App = () => {
     return (
         <View>
-           <KomponenUtama/>
+            <Text>Ini adalah Card Product</Text>
+           <Cardproduct/>
         </View>
     );
 };
 
-const GayaKomponen = () =>{
+const CardProduct = () =>{
     return (
     <View>
-        <Text style={style.text}>Ini adalah Gaya Komponen</Text>
-        <View 
-            style={{
-                width:100,
-                height:100,
-                backgroundColor: '#0abde3',
-                borderColor: '#5f27cd',
-                marginTop: 20,
-                marginLeft: 20,
-            }}
-        />
+        //<Text style={style.judul}>Card Product</Text>
+        <View style={style.box}>
+        <Image source={{uri: 'https://placeimg.com/188/107/tech'}} style={style.gambar} />
+        <Text style={style.heading2}>Macbook Pro 2019</Text>
+        <Text style={style.heading3}>Rp. 25.000.000</Text>
+        <Text style={style.lable}>Jakarta Barat</Text>
+        <View style={style.buttonview}>
+          <Text style={style.textbutton}>BELI</Text>
+        </View>
+        </View>
     </View>
     )
 };
 
 const style = StyleSheet.create({
-    text: {
-        fontSize: 18,
-    },
+    judul: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginLeft: 20,
+        
+     },
+     box: {
+       marginLeft: 20,
+       padding: 12,
+       backgroundColor: '#f2f2f2',
+       width: 212,
+       borderRadius: 8,
+     },
+     gambar:{
+       width: 188,
+       height: 107,
+       borderRadius: 8,
+     },
+     heading2:{
+       fontSize: 14, fontWeight: 'bold', marginTop: 16,
+     },
+     heading3:{
+       fontSize:12, fontWeight:'bold', color: 'orange', marginTop: 12,
+     },
+     lable:{
+       fontSize: 11,
+       fontWeight: 'normal',
+       marginTop: 8,
+     },
+     textbutton:{
+       fontSize: 14,
+       fontWeight: '600',
+       color: 'white',
+       textAlign: 'center',
+     },
+     buttonview:{
+       backgroundColor: 'green',
+       paddingTop: 6, paddingBottom: 6,
+       borderRadius: 20,
+       marginTop: 16,
+     },
+
 });
-
-const KomponenUtama = () =>{
-    return(
-        <View>
-             <Text> Halo Dunia, Saya Yakin Habib</Text>
-            <Ket/>
-            <Photo/>
-            <Text> </Text>
-            <Text> Tulis sesuatu dibawah ini</Text>
-            <TextInput style={{borderWidth: 1}} />
-            <BoxGreen/>
-            <Profile/>
-        </View>
-    )
-}
-
-const Ket = () => {
-    return <Text> Dibawah ini merupakan gambar dari arsitektur bangunan</Text>
-};
-
-const Photo = () => {
-    return (
-        <Image 
-            source={{uri: 'https://placeimg.com/100/100/arch'}}
-            style= {{width: 100, height: 100}}
-        />
-    );
-};
-
-class BoxGreen extends Component{
-    render(){
-        return <Text> Ini adalah component dari class component</Text>
-    }
-}
-
-class Profile extends Component{
-    render() {
-        return(
-            <View>
-                <Image source={{uri: 'https://placeimg.com/100/100/animals'}} 
-                style={{width: 100, height: 100}} />
-                <Text>Kucing Orange</Text>            
-            </View>
-        )
-    }
-}
 
 export default App;
